7bcd8fb1f7c62d1532342ce97a0ee9ab4ed3b542
Mlisande <melisande.malonga@gmail.com>
2022-08-09 11:41:17 +0200

    feat index post

--- microkham/app/Http/Controllers/PostController.php
+++ microkham/app/Http/Controllers/PostController.php
@@ -5,12 +5,36 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 
+
 class PostController extends Controller
 {
-   public function show(){ 
-    $post = Post::all();
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+  
+    public function index()
+    {
+
+        $posts = Post::latest()->get();
+
+        return view('posts.index', ['posts' => $posts]);
+
+    }
+
+        /**
+     * Display the specified resource.
+     * @param int $id
+     * @return \Illuminate\Http\View
+     * 
+     */ 
+   public function show($id){ 
+    // $post = Post::all();
     return view('posts.show', [
-        'post'=>$post[0]
+        'post'=>Post::findOrFail($id)
     ]);
 }
 }
+
+
--- microkham/database/factories/PostFactory.php
+++ microkham/database/factories/PostFactory.php
@@ -17,9 +17,9 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-          'description' => "Mon chat est trop mignon",
-          'img_url' => $this->faker->imageUrl(640, 480, 'cats'),
-          'user_id' => 1
+          'description' =>$this->faker->text(50),
+          'img_url' => 'https://source.unsplash.com/random/300x300/?otter',
+          'user_id' => $this->faker->randomElement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
         ];
     }
 }
