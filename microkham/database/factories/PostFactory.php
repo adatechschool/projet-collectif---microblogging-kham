8331d8807e192046914922832b9e6845f071a67e
anne barois <barois.anne@orange.fr>
2022-08-09 15:54:26 +0200

    Add Otter in Post Factory

--- microkham/database/factories/PostFactory.php
+++ microkham/database/factories/PostFactory.php
@@ -18,7 +18,29 @@ class PostFactory extends Factory
     {
         return [
           'description' =>$this->faker->text(50),
-          'img_url' => 'https://source.unsplash.com/random/300x300/?otter',
+          'img_url' => $this->faker->randomElement([
+                                                    'https://images.unsplash.com/photo-1597357821540-3dc1852ef417?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2613&q=80',
+                                                    'https://images.unsplash.com/photo-1633967920376-33b2d94f091f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+                                                    'https://images.unsplash.com/photo-1597378609416-47cc937b294c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80',
+                                                    'https://images.unsplash.com/photo-1589860170912-6b83cf6f8809?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1746&q=80',
+                                                    'https://images.unsplash.com/photo-1632755588732-6797a85f6d4b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1034&q=80',
+                                                    'https://images.unsplash.com/photo-1595854443827-9d1a1ae30f0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+                                                    'https://images.unsplash.com/photo-1626248156011-a278d3360625?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80',
+                                                    'https://images.unsplash.com/photo-1584381798516-5b2cbf2b1b5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+                                                    'https://images.unsplash.com/photo-1599236434423-d81f578c832a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+                                                    'https://images.unsplash.com/photo-1590691565311-747bdc916e9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80',
+                                                    'https://images.unsplash.com/photo-1580092242564-33c8fe326246?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2066&q=80',
+                                                    'https://images.unsplash.com/photo-1563155853-f7f8955967ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+                                                    'https://images.unsplash.com/photo-1599236449650-f2a86b592422?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+                                                    'https://images.unsplash.com/photo-1602061713885-ce043b10e560?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+                                                    'https://images.unsplash.com/photo-1625228077636-2ef3f23a39aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80',
+                                                    'https://images.unsplash.com/photo-1530701594694-da628544d738?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2537&q=80',
+                                                    'https://images.unsplash.com/photo-1619888759973-221cdd4f3652?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80',
+                                                    'https://images.unsplash.com/photo-1463978385905-88978d20502c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1778&q=80',
+                                                    'https://images.unsplash.com/photo-1631489356374-edaaab9834f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+                                                    'https://images.unsplash.com/photo-1597378609981-b46ae6a0ed40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1624&q=80',
+                                                    'https://images.unsplash.com/photo-1576727298631-92d3ed600ce9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80'
+                                                  ]),
           'user_id' => $this->faker->randomElement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
         ];
     }
